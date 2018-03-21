239624d468812c1b2c9b58995015a74d8cd06d30
haripriya23 <36726432+haripriya23@users.noreply.github.com>
2018-03-21 16:12:21 +0530

    Create power of a number .py

---
+++ power of a number .py
@@ -0,0 +1,8 @@
+def power(base,exp):
+    if(exp==1):
+        return(base)
+    if(exp!=1):
+        return(base*power(base,exp-1))
+base=int(input("Enter base: "))
+exp=int(input("Enter exponential: "))
+print("Result:",power(base,exp))
