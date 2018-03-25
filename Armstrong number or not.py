eca5a02d1716e592085813865a27f5757f8c7241
haripriya23 <36726432+haripriya23@users.noreply.github.com>
2018-03-25 14:25:05 +0530

    Create Armstrong number or not.py

---
+++ Armstrong number or not.py
@@ -0,0 +1,12 @@
+num=int(input("enter the number:"))
+sum = 0
+temp = num
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp //= 10
+if num == sum:
+    print("yes")
+else:
+    print("no")
+
