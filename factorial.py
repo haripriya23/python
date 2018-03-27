49e249c633803540d61d0750c3be8ec6e7116d13
haripriya23 <36726432+haripriya23@users.noreply.github.com>
2018-03-27 14:14:36 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number: "))
+factorial = 1
+if num < 0:
+    print("Please enter a positive integer")
+else:
+    for i in range(1, num + 1):
+        factorial *= i
+    print("The factorial of", num, "is", factorial)
